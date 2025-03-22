e78e6a81fc483b6333554ef8c659b53fffe7930c
Julio M Cruz <julio.cruz@eb-ms.net>
2025-03-22 03:30:35 -0400

    Update Docker deployment

--- DockerApps/WordPress/Docker/wp-config.php
+++ DockerApps/WordPress/Docker/wp-config.php
@@ -14,7 +14,7 @@ define( 'DB_USER', 'wordpressuser' );
 define( 'DB_PASSWORD', 'password' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost:/var/run/mysqld/mysqld.sock' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
